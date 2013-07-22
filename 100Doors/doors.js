5dd92c3fe7cae92c3fad02210828e365f39df1ee
= <jeff.foster@acm.org>
2013-07-22 12:56:56 +0100

    gree, doors defined, but empty

--- 100Doors/doors.js
+++ 100Doors/doors.js
@@ -1,10 +1,14 @@
 var assert = require('assert');
 
+var doors = [];
+
 var allDoorsAreClosed = function () {
   var allDoorsClosed = true;
   for (var door in doors) {
     allDoorsClosed = allDoorsClosed && door.closed;
   }
+
+  return allDoorsClosed;
 };
 
 
