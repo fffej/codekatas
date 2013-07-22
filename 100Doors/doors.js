509a102d9ded7838129c8a62b0db0a84c3b65166
= <jeff.foster@acm.org>
2013-07-22 13:10:10 +0100

    red: door should be toggled open

--- 100Doors/doors.js
+++ 100Doors/doors.js
@@ -35,13 +35,21 @@ var doorAt = function(doors,index) {
   return doors[index];
 };
 
+var toggle = function(door) {
+};
+
 
 var main = function () {
   var doors = createDoors();
  
   assert.equal(doorCount(doors), 100, "there should be 100 doors");
   assert(allDoorsAreClosed(doors), "all doors are closed");
-  assert(isClosed(doorAt(doors,0)), "door 0 is closed");
+
+  var door = doorAt(doors,0);
+  assert(isClosed(door), "door should be closed");
+
+  toggle(door);
+  assert(!isClosed(door), "door should be open");
 };
 
 main();
