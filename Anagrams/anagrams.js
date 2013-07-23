fcacb98570b49ebab5e49d5507ddedf729389b3a
= <jeff.foster@acm.org>
2013-07-23 09:19:57 +0100

    test passes

--- Anagrams/anagrams.js
+++ Anagrams/anagrams.js
@@ -1,7 +1,11 @@
 var assert = require('assert');
 
+var anagrams = function(word) {
+  return [''];
+};
+
 var test = function() {
-  assert.equals([''], anagrams(''));
+  assert.deepEqual([''], anagrams(''));
 }
 
 test();
