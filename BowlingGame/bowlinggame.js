b9c53a5183b3845b55ceb5006cd36434c5d128ab
= <jeff.foster@acm.org>
2013-07-23 13:59:08 +0100

    fail for the perfect game

--- BowlingGame/bowlinggame.js
+++ BowlingGame/bowlinggame.js
@@ -2,7 +2,14 @@
 
 var assert = require('assert');
 
+var bowl = function(hits) {
+
+};
+
 var test = function() {
+  var score = bowl(10,10,10,10,10,10,10,10,10,10);
+  assert.equal(300,score);
+
 };
 
 test();
