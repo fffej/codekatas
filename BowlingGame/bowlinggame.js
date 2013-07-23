5202daa0e2b55112a8eebcb0c69e9d2b6b3d146c
= <jeff.foster@acm.org>
2013-07-23 14:04:48 +0100

    failing test case for bowling a single frame

--- BowlingGame/bowlinggame.js
+++ BowlingGame/bowlinggame.js
@@ -17,8 +17,16 @@ var testInitialState = function() {
   assert.equal(0, game.currentScore);
 };
 
+var testBowlingSingleFrame = function() {
+  var game = new Game();
+  game.bowl(9,0);
+  assert.equal(2, game.currentFrame);
+};
+
 var test = function() {
   testInitialState();
+
+  testBowlingSingleFrame();
 };
 
 test();
