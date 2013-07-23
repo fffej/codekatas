5ec7399b78a66d9419ded3d82c4b3e5cb7b088de
= <jeff.foster@acm.org>
2013-07-23 09:22:13 +0100

    failing test for number of anagrams

--- Anagrams/anagrams.js
+++ Anagrams/anagrams.js
@@ -3,14 +3,19 @@ var assert = require('assert');
 var anagrams = function(word) {
   if (word.length <= 1) {
     return [word];
+  } else {
+
   }
 };
 
 var test = function() {
 
+  // base case
   assert.deepEqual([''], anagrams(''));
-
   assert.deepEqual(['a'], anagrams('a'));
+
+  var twoLetterAnagrams = anagrams('ab');
+  assert.equal(1, twoLetterAnagrams.length);
 }
 
 test();
