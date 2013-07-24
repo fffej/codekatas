97926e8fb3b49ce41a5a78e6ea1ed350d788e4dd
Jeff Foster <jeff.foster@acm.org>
2013-07-24 20:33:28 +0100

    possible change

--- CountCoins/countCoins.js
+++ CountCoins/countCoins.js
@@ -9,8 +9,13 @@ var testThereAreFourTypesOfCoins = function() {
   assert.equal(coins.length, 4);
 };
 
+var testPossibleChange = function() {
+  assert.equal(1, possibleChange(1).length);
+};
+
 var test = function() {
   testThereAreFourTypesOfCoins();
+  testPossibleChange();
 };
 
 test();
