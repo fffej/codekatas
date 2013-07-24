38a921a3d914ee7264258591be9764042eb5ced8
Jeff Foster <jeff.foster@acm.org>
2013-07-24 20:31:58 +0100

    define coins

--- CountCoins/countCoins.js
+++ CountCoins/countCoins.js
@@ -1,5 +1,9 @@
 var assert = require('assert');
 
+var getCoins = function() {
+  return [1,5,10,25];
+};
+
 var testThereAreFourTypesOfCoins = function() {
   var coins = getCoins();
   assert.equal(coins.length, 4);
