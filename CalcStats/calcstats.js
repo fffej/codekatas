876c262bba8e97aa4ef321023ae4f3521d8208de
Jeff Foster <jeff.foster@acm.org>
2013-07-24 07:51:52 +0100

    first failing test

--- CalcStats/calcstats.js
+++ CalcStats/calcstats.js
@@ -1,4 +1,12 @@
+var assert = require('assert');
+
+var testEmptyStats = function() {
+  var stats = calcStats([]);
+  assert.equal(undefined, stats.minValue);
+};
+
 var test = function() {
+  testEmptyStats();
 };
 
 test();
