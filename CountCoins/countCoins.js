a1c1a3bdef4e760dca923e0e84fbb3865a3fa32d
Jeff Foster <jeff.foster@acm.org>
2013-07-24 20:35:33 +0100

    test fails, why?

--- CountCoins/countCoins.js
+++ CountCoins/countCoins.js
@@ -4,6 +4,19 @@ var getCoins = function() {
   return [1,5,10,25];
 };
 
+var possibleChange = function(value,coins) {
+  coins = coins || getCoins();
+
+  var possibleChangeVals = [];
+  for(var i=0;i<coins.length;++i) {
+    if (i <= value) {
+      possibleChangeVals.push(value - i);
+    }
+  }
+
+  return possibleChangeVals;
+};
+
 var testThereAreFourTypesOfCoins = function() {
   var coins = getCoins();
   assert.equal(coins.length, 4);
