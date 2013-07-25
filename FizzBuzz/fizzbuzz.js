730e7c3235688e57fc43c98a23acce77998507c8
= <jeff.foster@acm.org>
2013-07-25 12:24:09 +0100

    fizz

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -1,6 +1,10 @@
 "use strict";
 var assert = require('assert');
 
+var fizz = function(v) {
+  return v % 3 == 0;
+};
+
 describe("fizzbuzz", function() {
   it("should fizz", function() {
     assert(fizz(3));
