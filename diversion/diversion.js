3e61146ddfe98bf7259df1efffa2e395df9903c4
= <jeff.foster@acm.org>
2013-07-25 08:42:43 +0100

    woo, bitsSet works, now to test for adjacent bits

--- diversion/diversion.js
+++ diversion/diversion.js
@@ -43,6 +43,7 @@ describe('Diversion', function() {
   it('returns the binary representation', function() {
     assert.deepEqual([], bitsSet(0));
     assert.deepEqual([0,1], bitsSet(2));
+    assert.deepEqual([1,0,1,0,0,0,0,1], bitsSet(133));
   });
 
   it('bit shift operations', function() {
