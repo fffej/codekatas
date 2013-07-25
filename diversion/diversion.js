149fb524412d0bfc2ce92c7a021836e11a64d772
= <jeff.foster@acm.org>
2013-07-25 08:23:49 +0100

    bit shift operations

--- diversion/diversion.js
+++ diversion/diversion.js
@@ -8,4 +8,10 @@ describe('Diversion', function() {
   it('has a bit that is set', function() {
     assert(bitset(1,1));
   });
+
+  it('bit shift operations', function() {
+    assert.equal(1, 1 << 0);
+    assert.equal(2, 1 << 1);
+    assert.equal(4, 1 << 2);
+  });
 });
