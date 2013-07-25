ef61cc55da657f4cc5beb996d460818f371fff88
= <jeff.foster@acm.org>
2013-07-25 08:21:21 +0100

    failing test

--- diversion/diversion.js
+++ diversion/diversion.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 
 describe('Diversion', function() {
-  it('1 + 1 should be two', function() {
-    assert.equal(2, 1 + 1);
+  it('has a bit that is set', function() {
+    assert(bitset(1,1));
   });
 });
