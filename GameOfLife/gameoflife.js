41816ac915005d1c1a4dbe190f46143517650e55
Jeff Foster <jeff.foster@acm.org>
2013-07-26 13:54:58 +0100

    and read in the rows and columns

--- GameOfLife/gameoflife.js
+++ GameOfLife/gameoflife.js
@@ -4,6 +4,14 @@ var assert = require('assert');
 
 var sampleInput = "4 8\n........\n....*...\n...**...\n........\n";
 
+var createGrid = function(s) {
+  var lines = s.split('\n');
+  var rc = lines[0].split(' ');
+  var rows = rc[0] | 0;
+  var cols = rc[1] | 0;
+
+  return { rows: rows, cols: cols };
+};
 
 describe('game of life', function() {
   describe('grid', function() {
