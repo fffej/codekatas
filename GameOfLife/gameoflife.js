be09c3b5b46e3a6d1d3144d98fa33b4e78c8b560
Jeff Foster <jeff.foster@acm.org>
2013-07-26 07:32:16 +0100

    failing test

---
+++ GameOfLife/gameoflife.js
@@ -0,0 +1,10 @@
+"use strict";
+var assert = require('assert');
+
+describe('Game of life', function() {
+  describe('cell', function() {
+    it('fewer than two neighbours dies', function() {
+      assert.fail('fail!');
+    });
+  });
+});
