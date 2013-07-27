97b8639af7ca0d9b232bb4d15099910a21b8b9af
Jeff Foster <jeff.foster@acm.org>
2013-07-27 18:56:28 +0100

    make the test pass

--- Haiku/haiku.js
+++ Haiku/haiku.js
@@ -1,6 +1,10 @@
 "use strict";
 var assert = require('assert');
 
+var syllableCount = function(word) {
+  return 1;
+};
+
 describe('haiku', function() {
   describe('syllables', function() {
     it('should be able to count syllables', function() {
