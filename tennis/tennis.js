4b0a1157d466d37a52c79e52563a1da33a2d3715
Jeff Foster <jeff.foster@acm.org>
2013-07-28 19:33:08 +0100

    failing test

--- tennis/tennis.js
+++ tennis/tennis.js
@@ -16,5 +16,12 @@ describe('tennis', function() {
       var game = new Game('Joe', 'Fred');
       assert.equal('Joe 0 - Fred 0', game.score());
     });
+
+    it ('game to love', function() {
+      var game = new Game('Joe', 'Fred');
+      game.serverPoint();
+  
+      assert.equal('Joe 15 - Fred 0', game.score());
+    });
   });
 });
