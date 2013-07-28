b61608133c105693a3a4ba49199c1b9f42e70118
Jeff Foster <jeff.foster@acm.org>
2013-07-28 20:06:06 +0100

    failing test case

--- tennis/tennis.js
+++ tennis/tennis.js
@@ -67,6 +67,9 @@ describe('tennis', function() {
       game.withServe();
       assert.equal('Joe A - Fred 40', game.score());
 
+      game.againstServe();
+      assert.equal('Joe 40 - Fred 40', game.score());
+
       game.withServe();
       assert.equal('Joe won', game.score());
     });
