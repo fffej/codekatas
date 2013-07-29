9d527e6c48719462d06a771768510626961785df
Jeff Foster <jeff.foster@acm.org>
2013-07-29 18:52:00 +0100

    failing test

--- MineField/minefield.js
+++ MineField/minefield.js
@@ -10,12 +10,21 @@ var Grid = function(w,h) {
 };
 
 describe('minefield', function() {
+  var sampleGrid = '3 4\n*...\n..*.\n....';
+
   describe('grid', function() {
     it('should be able to create a grid', function() {
       var grid = new Grid(5,6);
 
       assert.equal(grid.width, 5);
-      assert.equal(grid.height, 6);
+      assert.equal(grid.height,6);
+    });
+
+    it('should be able to create a grid from a string', function() {
+      var grid = createGridFromString(sampleGrid);
+
+      assert.equal(grid.width,4);
+      assert.equal(grid.height,3);
     });
   });
 });
