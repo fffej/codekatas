4a843647f1991fd18279565279e7193e15289ecb
Jeff Foster <jeff.foster@acm.org>
2013-07-30 18:35:15 +0100

    start of a failing acceptance test

---
+++ Reversi/reversi.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var assert = require('assert');
+
+describe('reversi', function() {
+  describe('acceptance test', function() {
+    it('should produce the expected output', function() {
+      assert.equal(sampleOutput, drive(sampleInput));
+    });
+  });
+});
