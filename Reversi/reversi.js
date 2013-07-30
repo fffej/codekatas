d46786d43f837bc1fe77193feb6255e64c6b832a
Jeff Foster <jeff.foster@acm.org>
2013-07-30 18:37:08 +0100

    add the sample input

--- Reversi/reversi.js
+++ Reversi/reversi.js
@@ -4,6 +4,19 @@ var assert = require('assert');
 
 describe('reversi', function() {
   describe('acceptance test', function() {
+
+    var sampleInput = '........\n' +
+                      '........\n' +
+                      '........\n' +
+                      '...BW...\n' +
+                      '...WB...\n' +
+                      '........\n' +
+                      '........\n' +
+                      '........\n' +
+                      'B\n';
+ 
+    var sampleOutput = '';
+
     it('should produce the expected output', function() {
       assert.equal(sampleOutput, drive(sampleInput));
     });
