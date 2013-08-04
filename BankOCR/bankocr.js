a9a7c00c021f7e15bb3020db8a7c3b115d19ecd7
Jeff Foster <jeff.foster@acm.org>
2013-08-04 19:16:29 +0100

    make the 'test' pass

--- BankOCR/bankocr.js
+++ BankOCR/bankocr.js
@@ -4,10 +4,14 @@ var assert = require('assert');
 
 describe('bank', function() {
 
-  var input = '';
+  var useCase1 = ' _  _  _  _  _  _  _  _  _ \n' + 
+                 '| || || || || || || || || |\n' +
+                 '|_||_||_||_||_||_||_||_||_|\n' +
+                 '';
 
   describe('input format', function() {
     it("consists of four lines", function() {
+      var input = useCase1;
       assert.equal(4, input.split('\n').length);
     });
   });
