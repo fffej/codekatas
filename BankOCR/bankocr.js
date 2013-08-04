c9a67c4e8ca52c08a85722d1fcb1f8ae64659e30
Jeff Foster <jeff.foster@acm.org>
2013-08-04 19:13:55 +0100

    start getting some tests in place regarding the input format

---
+++ BankOCR/bankocr.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var assert = require('assert');
+
+describe('bank', function() {
+
+  var input = '';
+
+  describe('input format', function() {
+    it("consists of four lines", function() {
+      assert.equal(4, input.split('\n').length);
+    });
+  });
+});
