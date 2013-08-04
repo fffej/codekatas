cdd22bc1c9aa72698d014934e33dcc4624d63118
Jeff Foster <jeff.foster@acm.org>
2013-08-04 19:17:55 +0100

    further document the format

--- BankOCR/bankocr.js
+++ BankOCR/bankocr.js
@@ -14,5 +14,13 @@ describe('bank', function() {
       var input = useCase1;
       assert.equal(4, input.split('\n').length);
     });
+
+    it("each line consists of 27 characters", function() {
+      var input = useCase1;
+      var lines = input.split('\n');
+      for(var i=0;i<3;++i) {
+        assert.equal(27, lines[i].length);
+      }
+    });
   });
 });
