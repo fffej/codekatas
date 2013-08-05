a0d730808a0d01ac85cc6768333511ec66ff840c
Jeff Foster <jeff.foster@acm.org>
2013-08-05 21:43:01 +0100

    I don't need these bonkers contortions, I just plain didnt understand what I was doing

--- JsCanvasGrid/grid.js
+++ JsCanvasGrid/grid.js
@@ -1,45 +1,41 @@
 // Daft contortion to avoid strict mode problems
+"use strict";
+
 var exportFn = function (grid,on,off,gridview) {
-  if (typeof(exports) !== 'undefined') {
-    exports.Grid = grid;
-    exports.On = on;
-    exports.Off = off;
-    exports.GridView = gridview;
-  }
 };
 
-(function() {
-  "use strict";
-  var On  = 1;
-  var Off = 0;
-  
-  var GridView = function(grid) {
-  
-  };
+var On  = 1;
+var Off = 0;
 
-  var Grid = function(w,h) {
-    var grid = [];
-    for (var i=0;i<h;++i) {
-      grid[i] = [];
-      for (var j=0;j<w;++j) {
-        grid[i].push(Off);
+var GridView = function(grid) {
+
+};
+ var Grid = function(w,h) {
+  var grid = [];
+  for (var i=0;i<h;++i) {
+    grid[i] = [];
+    for (var j=0;j<w;++j) {
+      grid[i].push(Off);
+    }
+  }
+   return {
+    width:  function() { return w; },
+    height: function() { return h; },
+    state: function(x,y) { return grid[x][y]; },
+    toggle: function(x,y) {
+      if (this.state(x,y) === On) {
+        grid[x][y] = Off;
+      } else {
+        grid[x][y] = On;
       }
     }
-
-    return {
-      width:  function() { return w; },
-      height: function() { return h; },
-      state: function(x,y) { return grid[x][y]; },
-      toggle: function(x,y) {
-        if (this.state(x,y) === On) {
-          grid[x][y] = Off;
-        } else {
-          grid[x][y] = On;
-        }
-      }
-    };
   };
+};
 
-  exportFn(Grid,On,Off,GridView);  
-}());
+if (typeof(exports) !== 'undefined') {
+  exports.Grid = Grid;
+  exports.On = On;
+  exports.Off = Off;
+  exports.GridView = GridView;
+}
 
