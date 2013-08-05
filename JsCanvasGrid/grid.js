df89ae40dfb5cf596ab88e9df8b2721890d1dba6
Jeff Foster <jeff.foster@acm.org>
2013-08-05 21:13:37 +0100

    tidy up exports, still failing

--- JsCanvasGrid/grid.js
+++ JsCanvasGrid/grid.js
@@ -1,5 +1,8 @@
 "use strict";
 
+var On  = 1;
+var Off = 0;
+
 var Grid = function(w,h) {
   return {
     width:  function() { return w; },
@@ -8,3 +11,5 @@ var Grid = function(w,h) {
 };
 
 exports.Grid = Grid;
+exports.On = On;
+exports.Off = Off;
