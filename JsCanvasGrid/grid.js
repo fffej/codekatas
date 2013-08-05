0f6cc70f6c367236c78301db333fc9abdaa16d67
Jeff Foster <jeff.foster@acm.org>
2013-08-05 21:35:26 +0100

    explicit export, use strict only where possible

--- JsCanvasGrid/grid.js
+++ JsCanvasGrid/grid.js
@@ -1,37 +1,43 @@
-"use strict";
-
-var On  = 1;
-var Off = 0;
-
-var GridView = function(grid) {
-
+// Daft contortion to avoid strict mode problems
+var exportFn = function (grid,on,off,gridview) {
+  exports.Grid = grid;
+  exports.On = on;
+  exports.Off = off;
+  exports.GridView = gridview;
 };
 
-var Grid = function(w,h) {
+(function() {
+  "use strict";
+  var On  = 1;
+  var Off = 0;
+  
+  var GridView = function(grid) {
+  
+  };
 
-  var grid = [];
-  for (var i=0;i<h;++i) {
-    grid[i] = [];
-    for (var j=0;j<w;++j) {
-      grid[i].push(Off);
-    }
-  }
-
-  return {
-    width:  function() { return w; },
-    height: function() { return h; },
-    state: function(x,y) { return grid[x][y]; },
-    toggle: function(x,y) {
-      if (this.state(x,y) === On) {
-        grid[x][y] = Off;
-      } else {
-        grid[x][y] = On;
+  var Grid = function(w,h) {
+    var grid = [];
+    for (var i=0;i<h;++i) {
+      grid[i] = [];
+      for (var j=0;j<w;++j) {
+        grid[i].push(Off);
       }
     }
-  };  
-};
 
-exports.Grid = Grid;
-exports.On = On;
-exports.Off = Off;
-exports.GridView = GridView;
+    return {
+      width:  function() { return w; },
+      height: function() { return h; },
+      state: function(x,y) { return grid[x][y]; },
+      toggle: function(x,y) {
+        if (this.state(x,y) === On) {
+          grid[x][y] = Off;
+        } else {
+          grid[x][y] = On;
+        }
+      }
+    };
+  };
+
+  exportFn(Grid,On,Off,GridView);  
+}());
+
