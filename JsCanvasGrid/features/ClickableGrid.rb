61109ec08108f14513e39c8c7c2cd627bf7c615c
Jeff Foster <jeff.foster@acm.org>
2013-08-05 20:45:56 +0100

    mark all tests have pending, need to understand a bit more about watir now

--- JsCanvasGrid/features/ClickableGrid.rb
+++ JsCanvasGrid/features/ClickableGrid.rb
@@ -2,7 +2,23 @@ require "rubygems"
 require "watir"
 
 Given /^that I have gone to the page$/ do
-  @browser = Wait::Chrome.new
-  @browser.goto("index.html")
+  # @browser = Wait::Chrome.new
+  # @browser.goto("index.html")
+  pending
 end
 
+When /^I click \((\d+),(\d+)\) in the grid$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it toggles colour$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^click \((\d+),(\d+)\) in the grid$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the colour is maintained$/ do
+  pending # express the regexp above with the code you wish you had
+end
