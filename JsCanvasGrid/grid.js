d65a23e1c97e21d17054bc33948b2a95cdf2fcdb
Jeff Foster <jeff.foster@acm.org>
2013-08-05 21:08:07 +0100

    get a brief understanding of how module exports work

--- JsCanvasGrid/grid.js
+++ JsCanvasGrid/grid.js
@@ -4,3 +4,4 @@ var Grid = function(w,h) {
   
 };
 
+exports.Grid = Grid;
--- JsCanvasGrid/gridtest.js
+++ JsCanvasGrid/gridtest.js
@@ -1,10 +1,11 @@
 "use strict";
 
 var assert = require('assert');
+var g = require('./grid.js');
 
 describe('grid', function() {
   it('has a width and height', function() {
-    var grid = new Grid(5,6);
+    var grid = new g.Grid(5,6);
 
     assert.equal(5, grid.width());
     assert.equal(6, grid.height());
