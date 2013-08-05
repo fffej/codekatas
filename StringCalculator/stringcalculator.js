3305609acce64c70a5a59ba328a2fe57bc654757
= <jeff.foster@acm.org>
2013-08-05 13:24:38 +0100

    passing test for multiple numbers

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -14,7 +14,12 @@ var StringCalculator = function() {
       }
 
       var nums = str.split(',');
-      return (nums[0]|0) + (nums[1]|0);
+      var sum = 0;
+      for (var i=0;i<nums.length;++i) {
+        sum += (nums[i]|0);
+      }
+
+      return sum;
     }
   };
 };
