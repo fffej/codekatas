dc87f1ddb53de2aadb4c5ec00c46ccc324da1925
Jeff Foster <jeff.foster@acm.org>
2013-08-05 20:59:47 +0100

    woo, figure out how to close the browser afterwards

--- JsCanvasGrid/features/ClickableGrid.rb
+++ JsCanvasGrid/features/ClickableGrid.rb
@@ -1,27 +1,32 @@
 require "rubygems"
 require "watir-webdriver"
 
+
 Given /^that I have started the server$/ do
-  pending 
+  # I'd use this to start the server
 end
 
 Given /^that I have gone to the page$/ do
   @browser = Watir::Browser.new
-  @browser.goto 'index.html'
+  @browser.goto 'http://localhost:8000/index.html'
 end
 
 When /^I click \((\d+),(\d+)\) in the grid$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  pending
 end
 
 Then /^it toggles colour$/ do
-  pending # express the regexp above with the code you wish you had
+  pending
 end
 
 When /^click \((\d+),(\d+)\) in the grid$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  pending
 end
 
 Then /^the colour is maintained$/ do
-  pending # express the regexp above with the code you wish you had
+  pending
+end
+
+After do |scenario|
+  @browser.close
 end
