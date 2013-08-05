6d9260b0a0bdef7c06e1b736c26efd3718c0190a
Jeff Foster <jeff.foster@acm.org>
2013-08-05 21:21:11 +0100

    make the test pass

--- JsCanvasGrid/grid.js
+++ JsCanvasGrid/grid.js
@@ -4,10 +4,26 @@ var On  = 1;
 var Off = 0;
 
 var Grid = function(w,h) {
+
+  var grid = [];
+  for (var i=0;i<h;++i) {
+    grid[i] = [];
+    for (var j=0;j<w;++j) {
+      grid[i].push(Off);
+    }
+  }
+
   return {
     width:  function() { return w; },
     height: function() { return h; },
-    state: function(x,y) { return Off; }
+    state: function(x,y) { return grid[x][y]; },
+    toggle: function(x,y) {
+      if (this.state(x,y) === On) {
+        grid[x][y] = Off;
+      } else {
+        grid[x][y] = On;
+      }
+    }
   };  
 };
 
