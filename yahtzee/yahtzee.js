7b37a41649067788f3aed8ccabaf61e35f41560c
= <jeff.foster@acm.org>
2013-08-06 14:37:02 +0100

    passing

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var Yahtzee = function() {
+  return {};
+};
+
 describe('yahtzee', function() {
   describe('five dice', function() {
     it('should consist of 5 values between 1 and 6', function() {
