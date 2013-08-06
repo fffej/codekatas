c0ce4aa7d7e8728e7b001820df19336d511f9871
= <jeff.foster@acm.org>
2013-08-06 14:45:05 +0100

    stub categories

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -2,7 +2,7 @@
 
 var assert = require('assert');
 
-var Yahtzee = function() {
+var Game = function() {
 
   var dice = [];
   for (var i=0;i<5;++i) {
@@ -16,12 +16,39 @@ var Yahtzee = function() {
   };
 };
 
-var categories = [];
+var Chance = function() {};
+var Yahtzee = function() {};
+var Single = function(n) {};
+var Pair = function() {};
+var TwoPairs = function() {};
+var ThreeOfAKind = function() {};
+var FourOfAKind = function() {};
+var SmallStraight = function() {};
+var LargeStraight = function() {};
+var FullHouse = function() {};
+
+var categories = [
+  new Chance(),
+  new Yahtzee(),
+  new Single(1),
+  new Single(2),
+  new Single(3),
+  new Single(4),
+  new Single(5),
+  new Single(6),
+  new Pair(),
+  new TwoPairs(),
+  new ThreeOfAKind(),
+  new FourOfAKind(),
+  new SmallStraight(),
+  new LargeStraight(),
+  new FullHouse()
+];
 
 describe('yahtzee', function() {
   describe('five dice', function() {
     it('should consist of 5 values between 1 and 6', function() {
-      var yahtzee = new Yahtzee();
+      var yahtzee = new Game();
       assert(yahtzee);
 
       assert.equal(5, yahtzee.dice().length);
