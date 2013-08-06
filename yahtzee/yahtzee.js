57506a87a276acfb7bdb992abb71bdc11342c6d3
= <jeff.foster@acm.org>
2013-08-06 15:01:33 +0100

    failing test case

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -130,6 +130,7 @@ describe('yahtzee', function() {
 
     it('two pairs', function() {
       assert.equal(12 + 10, new Game([6,6,5,5,1]).score(new TwoPairs()));
+      assert.equal(0, new Game([6,6,5,3,1]).score(new TwoPairs()));
     });
   });
 });
