3b818c117a8396610de28e3bb752c3300a97576a
= <jeff.foster@acm.org>
2013-08-06 14:42:06 +0100

    define categories

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -16,6 +16,8 @@ var Yahtzee = function() {
   };
 };
 
+var categories = [];
+
 describe('yahtzee', function() {
   describe('five dice', function() {
     it('should consist of 5 values between 1 and 6', function() {
