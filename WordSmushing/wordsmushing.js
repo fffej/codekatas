9c812d9ef2e28ff0e5b05974c821896ec4dc1e1d
= <jeff.foster@acm.org>
2013-08-09 16:49:58 +0100

    refactor

--- WordSmushing/wordsmushing.js
+++ WordSmushing/wordsmushing.js
@@ -2,18 +2,21 @@
 
 var assert = require('assert');
 
-var smush = function(a,b) {
+var canSmush = function(a,b) {
   return (a[a.length-1] === b[0]);
 };
 
 describe('word smushing', function() {
   describe('can smush', function() {
     it('should', function() {
-      assert(smush('it','to'));
+      assert(canSmush('it','to'));
     });
 
     it('shouldn\'t', function() {
-      assert(!smush('be','to'));
+      assert(!canSmush('be','to'));
     });
+  });
+
+  describe('smush together', function() {
   });
 });
