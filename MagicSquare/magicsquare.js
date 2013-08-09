9ed717a894d23352c5d0f79084c7715b27713082
= <jeff.foster@acm.org>
2013-08-09 08:29:06 +0100

    pass

--- MagicSquare/magicsquare.js
+++ MagicSquare/magicsquare.js
@@ -2,6 +2,12 @@
 
 var assert = require('assert');
 
+var MagicSquare = function(size) {
+  return {
+
+  };
+};
+
 describe('magic square', function() {
   it('exists', function() {
     assert(new MagicSquare(3));
