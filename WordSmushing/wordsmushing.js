5beacfcfe31b7488eb3727d1e4dba04166f544ab
Jeff Foster <jeff.foster@acm.org>
2013-08-10 20:39:35 +0100

    better fail

--- WordSmushing/wordsmushing.js
+++ WordSmushing/wordsmushing.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var smush = function(a,b) {
+
+};
+
 describe('word smushing', function() {
   describe('two work smushing', function() {
     it('should smush', function() {
