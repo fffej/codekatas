d9f6b71dab4613dd1f1bd676b7706f87e8977f5f
Jeff Foster <jeff.foster@acm.org>
2013-08-10 20:41:30 +0100

    fail

--- WordSmushing/wordsmushing.js
+++ WordSmushing/wordsmushing.js
@@ -10,9 +10,14 @@ var smush = function(a,b) {
 
 describe('word smushing', function() {
   describe('two work smushing', function() {
-    it('should smush', function() {
+    it('should smush two equal words', function() {
       var smushed = smush('to','to');
       assert.equal('to', smushed);
+    });
+
+    it('should smush two letter words', function() {
+      var smushed = smush('it', 'to');
+      assert.equal('ito', smushed);
     });
   });
 });
