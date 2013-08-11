783e97ac4dcffbeb1c1ed173414dc8c4f340b7c4
Jeff Foster <jeff.foster@acm.org>
2013-08-11 20:49:22 +0100

    first failing test

---
+++ Range/range.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var assert = require('assert');
+
+describe('range', function() {
+  describe('creation', function() {
+    it('exists', function() {
+      assert(new Range);
+    });
+  });
+});
