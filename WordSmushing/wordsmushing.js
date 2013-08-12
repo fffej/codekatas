04d5dbe723c10df93e8670e7bffb90cf8658abe4
Jeff Foster <jeff.foster@acm.org>
2013-08-12 21:08:01 +0100

    first test (passes)

---
+++ WordSmushing/wordsmushing.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var assert = require('assert');
+
+
+var largestString = function(words) {
+
+};
+
+describe('words smushing', function() {
+  describe('largest string', function() {
+    it('is not defined for 0 length arrays', function() {
+      assert(!largestString([]));
+    });
+  });
+});
