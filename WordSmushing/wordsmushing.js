ecb9b8f64b40279e8407857dad91f6459f7c5d45
Jeff Foster <jeff.foster@acm.org>
2013-08-12 21:19:39 +0100

    failing test case

--- WordSmushing/wordsmushing.js
+++ WordSmushing/wordsmushing.js
@@ -4,7 +4,11 @@ var assert = require('assert');
 
 
 var largestString = function(words) {
-
+  if (words && words.length > 1) {
+    return words.reduce(function(x,y) {
+      return Math.max(x,y.length); 
+    }, words[0].length);
+  }
 };
 
 describe('words smushing', function() {
@@ -14,7 +18,13 @@ describe('words smushing', function() {
     });
 
     it('returns the length of the longest string', function() {
-      assert.equal(5, largestString['a','b','bc','defgh']);
+      assert.equal(5, largestString(['a','b','bc','defgh']));
+    });
+  });
+
+  describe('eliminate wholly contains substrings', function() {
+    it('duplicates are eliminated', function() {
+      assert.equal(1, eliminateSubstrings(['test','testing']).length);
     });
   });
 });
