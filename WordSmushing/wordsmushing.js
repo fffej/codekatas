4418589bccd0501b58809a463a4f9971dca18709
Jeff Foster <jeff.foster@acm.org>
2013-08-12 19:07:58 +0100

    define test for smush2

--- WordSmushing/wordsmushing.js
+++ WordSmushing/wordsmushing.js
@@ -6,12 +6,22 @@ var smush = function(a,b) {
   return 'ito';
 };
 
+var smush2 = function(a,b) {
+
+};
 
 describe('word smushing', function() {
   describe('base cases', function() {
     it('should find the minimal smush', function() {
       var smushed = smush('it', 'to');
       assert.equal('ito', smushed);
+    });
+  });
+
+  describe('smush2', function() {
+    it('should smush2 in the order specified', function() {
+      var smushed = smush2('to', 'it');
+      assert.equal('toit', smushed);
     });
   });
 });
