eb8436bcf152e761fc7131d009aecd6cb25983f1
Jeff Foster <jeff.foster@acm.org>
2013-08-12 21:52:09 +0100

    passing test

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -2,6 +2,14 @@
 
 var assert = require('assert');
 
+var Dice = function() {
+  this.roll = function() {
+    return Math.random() * 6 + 1;
+  };
+
+  return this;
+};
+
 describe('yahtzee', function() {
    describe('dice', function() {
      it('rolls a number between 1 and 6', function() {
