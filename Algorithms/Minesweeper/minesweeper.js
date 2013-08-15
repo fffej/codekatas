a21aed2c6b94864195515503ca0bddd5dcee6e87
Jeff Foster <jeff.foster@acm.org>
2013-08-15 21:24:09 +0100

    fail

--- Algorithms/Minesweeper/minesweeper.js
+++ Algorithms/Minesweeper/minesweeper.js
@@ -16,5 +16,11 @@ describe('minesweeper', function() {
       var grid = new Minesweeper(grid1);
       assert(grid);
     });
+
+    it('width and height are read correctly', function() {
+      var grid = new Minesweeper(grid2);
+      assert.equal(3,grid.height);
+      assert.equal(5,grid.width);
+    });
   });
 });
