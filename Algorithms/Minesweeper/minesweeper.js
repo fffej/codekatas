592cae32c2c3d30e5f13301a4be99fea0c5e5141
Jeff Foster <jeff.foster@acm.org>
2013-08-15 21:22:00 +0100

    first failing test

---
+++ Algorithms/Minesweeper/minesweeper.js
@@ -0,0 +1,15 @@
+"use strict";
+var assert = require('assert');
+
+describe('minesweeper', function() {
+
+  var grid1 = '4 4\n*...\n....\n....\n.*..\n';
+  var grid2 = '3 5\n**...\n.....\n.*...\n';
+
+  describe('can be created from string', function() {
+    it('can create from string', function() {
+      var grid = new Minesweeper(grid1);
+      assert(grid);
+    });
+  });
+});
