1e506f8d5e99d9318e3f1636ce75c57aa32313f3
= <jeff.foster@acm.org>
2013-08-16 08:53:39 +0100

    first simple pass

--- GameOfLife/gameoflife.js
+++ GameOfLife/gameoflife.js
@@ -1,10 +1,15 @@
 "use strict";
 var assert = require('assert');
 
+var Cell = {
+  DEAD: 0,
+  ALIVE: 1
+};
+
 describe('game of life', function() {
   describe('states', function() {
     it('dead or alive', function() {
-      assert(Cell.DEAD);
+      assert(Cell.DEAD !== undefined);
       assert(Cell.ALIVE);
       assert(Cell.DEAD !== Cell.ALIVE);
     });
