a3ed136e20d2430005cc33d899a6c9d4e524302e
= <jeff.foster@acm.org>
2013-08-20 15:58:14 +0100

    pass, skip refactor

--- WordSmushing/wordsmush.js
+++ WordSmushing/wordsmush.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var smush2 = function(a,b) {
+  return a + b;
+};
+
 describe('word smush', function() {
   describe('smush two works', function() {
     it('should concatenate words with no overlaps', function() {
