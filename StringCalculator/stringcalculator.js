8f6142b3136cbc2e519dd1f399c34bd97aa32a90
= <jeff.foster@acm.org>
2013-08-21 17:04:03 +0100

    pass

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var StringCalculator = function() {
+  return this;
+};
+
 describe('string calculator', function() {
   it('exists', function() {
     assert(new StringCalculator());
