d7922e9de829914d4a2f9b7e309126b202c9b5f4
Jeff Foster <jeff.foster@acm.org>
2013-08-22 19:39:46 +0100

    i know how to extend prototypes (probably a bad idea)

--- Algorithms/BinarySearch/binarysearch.js
+++ Algorithms/BinarySearch/binarysearch.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert'); 
 
+Array.prototype.binarySearch = function() {
+  return -1;
+};
+
 describe('binary search', function() {
   it('doesnt find anything in an empty list', function() {
     assert.equal(-1, [].binarySearch(7));
