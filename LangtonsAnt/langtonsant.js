c102a4c873fa2cabb9c585327a59ca3f9776d5ee
Jeff Foster <jeff.foster@acm.org>
2013-08-22 19:09:34 +0100

    add file

---
+++ LangtonsAnt/langtonsant.js
@@ -0,0 +1,10 @@
+"use strict";
+var assert = require('assert');
+
+describe('infinite grid', function() {
+  describe('grid', function() {
+    it('exists', function() {
+      assert(new Plane());
+    });
+  });
+});
