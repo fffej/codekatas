2804f38619a9219acac749c52fb4fd0f74cfbf8a
Jeff Foster <jeff.foster@acm.org>
2013-08-22 19:12:19 +0100

    fail

--- LangtonsAnt/langtonsant.js
+++ LangtonsAnt/langtonsant.js
@@ -2,13 +2,19 @@
 var assert = require('assert');
 
 var Plane = function() {
-
+  return this;
 };
 
 describe('infinite grid', function() {
   describe('grid', function() {
     it('exists', function() {
       assert(new Plane());
+    });
+  });
+
+  describe('ant', function() {
+    it('exists', function() {
+      assert(new Ant());
     });
   });
 });
