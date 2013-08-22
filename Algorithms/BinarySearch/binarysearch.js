2f9bdd229e49103fdcd8f486a3f41db0a333bcfd
Jeff Foster <jeff.foster@acm.org>
2013-08-22 19:46:03 +0100

    sketch

--- Algorithms/BinarySearch/binarysearch.js
+++ Algorithms/BinarySearch/binarysearch.js
@@ -2,12 +2,27 @@
 
 var assert = require('assert'); 
 
-Array.prototype.binarySearch = function() {
-  return -1;
+Array.prototype.binarySearch = function(n) {
+
+  var O = Object(this);
+
+  var f = function(lo,hi) {
+    var mid = Math.floor((lo + hi) / 2);
+    if (O[mid] === n) {
+      return mid;
+    } 
+    return -1;
+  };
+
+  return f(0, O.length);
 };
 
 describe('binary search', function() {
   it('doesnt find anything in an empty list', function() {
     assert.equal(-1, [].binarySearch(7));
+  });
+
+  it('finds a single element', function() {
+    assert.equal(0, [0].binarySearch(0));
   });
 });
