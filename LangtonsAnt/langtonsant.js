99429394747c7a9c8c94c762ddae72af2626e29e
Jeff Foster <jeff.foster@acm.org>
2013-08-22 19:10:22 +0100

    make test pass

--- LangtonsAnt/langtonsant.js
+++ LangtonsAnt/langtonsant.js
@@ -1,6 +1,10 @@
 "use strict";
 var assert = require('assert');
 
+var Plane = function() {
+
+};
+
 describe('infinite grid', function() {
   describe('grid', function() {
     it('exists', function() {
