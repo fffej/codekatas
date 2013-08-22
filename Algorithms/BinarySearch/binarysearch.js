6cbba4873c533a8368c30be162d2a093b9faf28d
Jeff Foster <jeff.foster@acm.org>
2013-08-22 19:46:37 +0100

    i expected that to pass, and it did

--- Algorithms/BinarySearch/binarysearch.js
+++ Algorithms/BinarySearch/binarysearch.js
@@ -25,4 +25,8 @@ describe('binary search', function() {
   it('finds a single element', function() {
     assert.equal(0, [0].binarySearch(0));
   });
+
+  it('finds the middle element', function() {
+    assert.equal(1, [1,2,3].binarySearch(2));
+  });
 });
