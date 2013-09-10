136ba601a425cc614dd7f113fe47ea66ce0ca325
= <jeff.foster@acm.org>
2013-09-10 15:49:23 +0100

    failing test case

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -28,6 +28,6 @@ describe('yahtzee', function() {
   it('roll five numbers', function() {
      var yahtzee = new Yahtzee();
      yahtzee.roll(1,2,3,4,5);
-     assert.equal(1 + 2 + 3 + 4 + 5, yahtzee.score());
+     assert.equal(1 + 2 + 3 + 4 + 5, yahtzee.score(Category.Chance));
   });
 });
