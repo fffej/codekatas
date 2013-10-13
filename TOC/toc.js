35bf6eba88b9b9352045ec918a3a3a77c993e8a0
Jeff Foster <jeff.foster@acm.org>
2013-10-13 18:37:32 +0100

    better failing test

--- TOC/toc.js
+++ TOC/toc.js
@@ -1,9 +1,14 @@
 "use strict";
 
 var rollDice = function() {
-    return Math.random() * 6 + 1 | 0;
+  return Math.random() * 6 + 1 | 0;
 }
 
+var createStage = function() {
+
+};
+
 if (typeof('')) {
-    exports.rollDice = rollDice;
+  exports.rollDice = rollDice;
+  exports.createStage = createStage;  
 }
