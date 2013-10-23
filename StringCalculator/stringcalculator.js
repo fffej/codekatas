ef3ac64f9e3e3d30944935d937703862d6a48b7c
= <jeff.foster@acm.org>
2013-10-23 16:39:53 +0100

    pass

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var add = function(str) {
+    return 0;
+};
+
 describe('string calculator', function() {
     describe('add', function() {
 	it('should return a 0 for empty string', function() {
