b4ed32c0082a32d5bc4203d510bdf595628f8812
= <jeff.foster@acm.org>
2013-10-23 16:43:19 +0100

    passing test

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -5,7 +5,13 @@ var assert = require('assert');
 var add = function(str) {
     if (str.length === 0) return 0;
 
-    return str|0;
+    var sum = 0;
+    var nums = str.split(',');
+    for (var i=0;i<nums.length;++i) {
+	sum += nums[i] | 0;
+    }
+
+    return sum;
 };
 
 describe('string calculator', function() {
@@ -19,7 +25,7 @@ describe('string calculator', function() {
 	});
 
 	it('should return sum for numbers separated by ,', function() {
-	    assert.equal(2, add('1,2'));
+	    assert.equal(3, add('1,2'));
 	});
     });
 });
