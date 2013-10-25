27f6929e898d01bff14b4683eef349122660d460
= <jeff.foster@acm.org>
2013-10-25 11:05:13 +0100

    write first failing test

--- MineField/MineSweeperTest.cs
+++ MineField/MineSweeperTest.cs
@@ -6,5 +6,29 @@ namespace Fatvat.Katas.MineSweeper
     [TestFixture]
     public class MineSweeperTest
     {
+        // ReSharper disable InconsistentNaming
+        [Test]
+        public void Given_Simplest_MineField_Can_ProduceOutput()
+        {
+            const string mineFieldInput = "1 1\n*";
+            const string expectedOutput = "*\n";
+
+            Assert.That(MineField.Read(mineFieldInput).Display(), Is.EqualTo(expectedOutput));
+        }
+
+        // ReSharper restore InconsistentNaming
+    }
+
+    public class MineField
+    {
+        public static MineField Read(string input)
+        {
+            return new MineField();
+        }
+
+        public string Display()
+        {
+            return "";
+        }
     }
 }
