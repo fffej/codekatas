200089194a798cc89878917b66a67b96f4e17e6c
Jeff Foster <jeff.foster@acm.org>
2013-10-29 07:12:58 +0000

    simplest possible pass

--- CowsAndBulls/CowsAndBulls.js
+++ CowsAndBulls/CowsAndBulls.js
@@ -2,6 +2,13 @@
 
 var assert = require('assert');
 
+var calculateScore = function(guess, secret) {
+    return {
+	cows: 0,
+	bulls: 0
+    };
+};
+
 describe('cows and bulls', function() {
     describe('scoring', function() {
 	it('scores nothing when no matches', function() {
