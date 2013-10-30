053262f6c36c7feeb482c504564b48d7bc6de5c0
= <jeff.foster@acm.org>
2013-10-30 15:32:34 +0000

    Simplify the code a wee bit

--- CowsAndBulls/CowsAndBulls.hs
+++ CowsAndBulls/CowsAndBulls.hs
@@ -4,27 +4,21 @@ data Row = Row Int Int Int Int deriving (Show,Eq)
 
 data Score = Score Int Int deriving (Show,Eq)
 
+data Player = Player [Row] deriving (Show,Eq)
+
+data GameState = GameState Row Player deriving (Show,Eq)
+
 rowToList :: Row -> [Int]
 rowToList (Row a b c d) = [a,b,c,d]
 
 score :: Row -> Row -> Score
 score a b = Score bulls cows
     where
-      bulls = calculateBulls a b 
-      cows = calculateCows a b
-
-calculateCows :: Row -> Row -> Int
-calculateCows a b = length $ filter (\(x,y) -> y `elem` xs) nonBulls
-    where
-      xs = rowToList a
+      xs = rowToList a 
       ys = rowToList b
       nonBulls = filter (uncurry (/=)) (zip xs ys)
-
-calculateBulls :: Row -> Row -> Int
-calculateBulls a b = length $ filter id (zipWith (==) xs ys)
-    where
-      xs = rowToList a
-      ys = rowToList b
+      cows = length $ filter (\(x,y) -> y `elem` xs) nonBulls
+      bulls = length $ filter id (zipWith (==) xs ys)
 
 allRows :: [Row]
 allRows = [Row a b c d | a <- [0..9], b <- [0..9], c <- [0..9], d <- [0..9], distinct a b c d]
@@ -36,15 +30,11 @@ allRows = [Row a b c d | a <- [0..9], b <- [0..9], c <- [0..9], d <- [0..9], dis
 removeBadGuesses :: Row -> Score -> [Row] -> [Row]
 removeBadGuesses r s = filter (\row -> score row r == s)
 
-data Player = Player [Row] deriving (Show,Eq)
-
 defaultPlayer :: Player
 defaultPlayer = Player allRows
 
 gameInProgress :: GameState -> Bool
 gameInProgress (GameState secret p) = score (lastGuess p) secret /= Score 4 0 
-
-data GameState = GameState Row Player deriving (Show,Eq)
 
 lastGuess :: Player -> Row
 lastGuess (Player xs) = head xs
