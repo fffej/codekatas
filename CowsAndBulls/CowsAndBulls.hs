e98a77ab80b1b27825d988da277ac452b15f8ec8
= <jeff.foster@acm.org>
2013-10-30 15:46:52 +0000

    Use a closure to eliminate the code smell GameState
    The reason that it is a smell is that we are only updating one part of
    it (secret was constant)

--- CowsAndBulls/CowsAndBulls.hs
+++ CowsAndBulls/CowsAndBulls.hs
@@ -5,7 +5,6 @@ import Data.List
 data Row = Row Int Int Int Int deriving (Show,Eq)
 data Score = Score Int Int deriving (Show,Eq)
 data Player = Player [Row] deriving (Show,Eq)
-data GameState = GameState Row Player deriving (Show,Eq)
 
 rowToList :: Row -> [Int]
 rowToList (Row a b c d) = [a,b,c,d]
@@ -28,23 +27,20 @@ allRows = filter distinct [Row a b c d | a <- [0..9], b <- [0..9], c <- [0..9], 
 removeBadGuesses :: Row -> Score -> [Row] -> [Row]
 removeBadGuesses r s = filter (\row -> score row r == s)
 
-gameInProgress :: GameState -> Bool
-gameInProgress (GameState secret p) = score (lastGuess p) secret /= Score 4 0 
-
 lastGuess :: Player -> Row
 lastGuess (Player xs) = head xs
 
 remaining :: Player -> [Row]
 remaining (Player xs) = xs
 
-makeGuess :: GameState -> GameState
-makeGuess (GameState secret player) = GameState secret newPlayer
-    where
-      guesses = remaining player
-      guess = head guesses
-      newPlayer = Player (removeBadGuesses guess (score guess secret) guesses)
-
 playGame :: Row -> Int
-playGame row = length $ takeWhile gameInProgress games 
+playGame secret = length $ takeWhile gameInProgress games 
     where
-      games = iterate makeGuess (GameState row (Player allRows))
+      games = iterate makeGuess (Player allRows)
+      gameInProgress player = score (lastGuess player) secret /= Score 4 0
+      makeGuess player = newPlayer
+          where
+            guesses = remaining player
+            guess = head guesses
+            newPlayer = Player (removeBadGuesses guess (score guess secret) guesses)
+
