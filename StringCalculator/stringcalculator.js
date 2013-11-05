b99929901270e04ad1d4278d9a3f33d7c50088eb
Jeff Foster <jeff.foster@red-gate.com>
2013-11-05 15:18:33 +0000

    fail

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -10,4 +10,8 @@ describe('string calculator', function() {
     it('should simply add', function() {
 	assert.equal(1, add('1'));
     });
+
+    it('should be identity for single numbers', function() {
+	assert.equal(123, add('123'));
+    });
 });
