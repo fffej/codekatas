4f42e2ee98cf7bdafb23cbd84dcc5b34c5ec2512
Jeff Foster <jeff.foster@acm.org>
2013-11-06 07:46:34 +0000

    boring pass

--- Anagrams/anagrams.js
+++ Anagrams/anagrams.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var anagrams = function() {
+    return ['a'];
+};
+
 describe('anagrams', function() {
     it('anagrams of a single character', function() {
 	var anagramsOfA = anagrams('a');
