65abecf885d5adcdb41dc9a84e1cca0ada17ecaa
Jeff Foster <jeff.foster@acm.org>
2013-11-06 07:38:23 +0000

    pass

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var fizzbuzz = function(i) {
+    return 1;
+};
+
 describe('fizz buzz', function() {
     it('numbers not divisible by 3 or 5 should be identity', function() {
 	assert.equal(1, fizzbuzz(1));
