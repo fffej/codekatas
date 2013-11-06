d5ed5c9368eff8c66e328f5f373314fd87e18c53
= <jeff.foster@acm.org>
2013-11-06 11:32:24 +0000

    tests pass

--- CalcStats/calcstats.js
+++ CalcStats/calcstats.js
@@ -13,4 +13,12 @@ describe('sequence processing', function() {
 	assert.equal(0, statistics([]).count);
 	assert.equal(3, statistics([1,2,3]).count);
     });
+
+    it('avg, min and max should be undefined for empty lists', function() {
+	var stats = statistics([]);
+
+	assert.equal(undefined, stats.minimum);
+	assert.equal(undefined, stats.maximum);
+	assert.equal(undefined, stats.average);
+    });
 });
