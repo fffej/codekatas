2a4f9e0e2131b30c46706b661edf63cdf1a00d1d
= <jeff.foster@acm.org>
2013-11-06 11:38:12 +0000

    simplest possible pass

--- CalcStats/calcstats.js
+++ CalcStats/calcstats.js
@@ -10,7 +10,8 @@ var statistics = function(s) {
     return {
 	count: s.length,
 	maximum: s.length === 0 ? undefined : s.reduce(maxFn, s[0]),
-	minimum: s.length === 0 ? undefined : s.reduce(minFn, s[0])
+	minimum: s.length === 0 ? undefined : s.reduce(minFn, s[0]),
+	average: s.length === 0 ? undefined : 0
     };
 };
 
