3b5d22069ec926c4736fa82fc0edb3b873fb2058
= <jeff.foster@acm.org>
2013-11-07 13:30:45 +0000

    boring pass

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -9,9 +9,10 @@ var valueIs = function(x) {
 var sum = function(x,y) { return x + y; };
 
 var Categories = {
-    Chance: function(dice) { return dice.reduce(sum); },
-    Ones:   function(dice) { return dice.filter(valueIs(1)).reduce(sum); },
-    Twos:   function(dice) { return dice.filter(valueIs(2)).reduce(sum); }
+    Chance:  function(dice) { return dice.reduce(sum); },
+    Ones:    function(dice) { return dice.filter(valueIs(1)).reduce(sum); },
+    Twos:    function(dice) { return dice.filter(valueIs(2)).reduce(sum); },
+    Threes:  function(dice) { return dice.filter(valueIs(3)).reduce(sum); }
 };
 
 var score = function(category, a,b,c,d,e) {
@@ -32,6 +33,11 @@ describe('yahtzee', function() {
 	it('should score \'twos\' as the sum of those matching twos', function() {
 	    assert.equal(2, score(Categories.Twos, 1,1,1,2,3));
 	});
+
+	it('should score \'threes\' as the sum of those matching threes', function() {
+	    assert.equal(3, score(Categories.Threes, 1,1,1,2,3));
+	});
+
 
     });
 });
