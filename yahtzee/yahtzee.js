126a2c45e24a76451577513f12b648567b594082
= <jeff.foster@acm.org>
2013-11-07 13:27:09 +0000

    refactor

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -9,13 +9,15 @@ var Categories = {
 
 var score = function(category, a,b,c,d,e) {
 
+    var dice = [a,b,c,d,e];
+
     var sum = function(x,y) { return x + y; };
 
     if (category === Categories.Chance) {
-	return a + b + c + d + e;
+	return dice.reduce(sum);
     }
     else if (category === Categories.Ones) {
-	return [a,b,c,d,e].filter(function(x) { return x === 1 }).reduce(sum);
+	return dice.filter(function(x) { return x === 1 }).reduce(sum);
     }
 };
 
