6a59b5b34491d6a5cb76dfbd3d8be5635c7a8b5f
= <jeff.foster@acm.org>
2013-11-07 13:23:55 +0000

    fail

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -8,8 +8,12 @@ var score = function(a,b,c,d,e) {
 
 describe('yahtzee', function() {
     describe('scoring', function() {
-	it('should score change as the sum of the dice', function() {
+	it('should score chance as the sum of the dice', function() {
 	    assert.equal(1+2+3+4+5, score(1,2,3,4,5));
+	});
+
+	it('should score \'ones\' as the sum of those matching ones', function() {
+	    assert.equal(3, score(1,1,1,2,3));
 	});
     });
 });
