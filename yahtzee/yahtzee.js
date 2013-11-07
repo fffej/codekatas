d849e15c7c3311c7cd87f8fe4193925b34e80e04
= <jeff.foster@acm.org>
2013-11-07 13:24:37 +0000

    refactor to add parameter

--- yahtzee/yahtzee.js
+++ yahtzee/yahtzee.js
@@ -2,18 +2,23 @@
 
 var assert = require('assert');
 
-var score = function(a,b,c,d,e) {
+var Categories = {
+    Chance: 0,
+    Ones:   1
+};
+
+var score = function(category, a,b,c,d,e) {
     return a + b + c + d + e;
 };
 
 describe('yahtzee', function() {
     describe('scoring', function() {
 	it('should score chance as the sum of the dice', function() {
-	    assert.equal(1+2+3+4+5, score(1,2,3,4,5));
+	    assert.equal(1+2+3+4+5, score(Categories.Chance, 1,2,3,4,5));
 	});
 
 	it('should score \'ones\' as the sum of those matching ones', function() {
-	    assert.equal(3, score(1,1,1,2,3));
+	    assert.equal(3, score(Categories.Ones, 1,1,1,2,3));
 	});
     });
 });
