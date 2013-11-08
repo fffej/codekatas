7e40b8d74ae00ebc0d16c22c609aaa31b2bfc5f3
= <jeff.foster@acm.org>
2013-11-08 09:47:04 +0000

    simple pass

--- Prelude/prelude.js
+++ Prelude/prelude.js
@@ -2,10 +2,20 @@
 
 var assert = require('assert');
 
+var id = function(x) { return x; };
+
+var map = function(arr,f) {
+    return arr;
+};
+
 describe('prelude', function() {
     describe('map', function() {
 	it('should preserve []', function() {
 	    assert.deepEqual([], map([], id));
+	});
+
+	it('should preserve array', function() {
+	    assert.deepEqual([1,2,3], map([1,2,3], id));
 	});
     });
 });
