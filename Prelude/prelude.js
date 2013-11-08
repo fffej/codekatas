4722d7630a51d673f293001be13ed46f1f8ec94b
= <jeff.foster@acm.org>
2013-11-08 09:52:55 +0000

    need to start with a list implementation

--- Prelude/prelude.js
+++ Prelude/prelude.js
@@ -2,28 +2,14 @@
 
 var assert = require('assert');
 
-var id = function(x) { return x; };
-
-var map = function(arr,f) {
-    var r = [];
-    for (var i=0;i<arr.length;++i) {
-	r.push(f(arr[i]));
-    }
-    return r;
+var List = {
+    empty: new Cons()
 };
 
 describe('prelude', function() {
-    describe('map', function() {
-	it('should preserve []', function() {
-	    assert.deepEqual([], map([], id));
-	});
-
-	it('should preserve array', function() {
-	    assert.deepEqual([1,2,3], map([1,2,3], id));
-	});
-
-	it('should apply function', function() {
-	    assert.deepEqual([1,2,3], map([0,1,2], function(x) { return x + 1; }));
+    describe('lists', function() {
+	it('should make an empty list', function() {
+	    assert.equal(List.empty, makeList());
 	});
     });
 });
