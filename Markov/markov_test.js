f2c82f11f4f04b3aa096ce1c8e02c0ddd529164d
Jeff Foster <jeff.foster@acm.org>
2013-11-10 14:27:51 +0000

    failing test case

--- Markov/markov_test.js
+++ Markov/markov_test.js
@@ -5,9 +5,10 @@ var markov = require('./markov.js');
 
 describe('Markov chain', function() {
     describe('building model', function() {
-	it('should exist', function() {
+	it('should provide entries', function() {
 	    var model = markov.buildModel('the quick brown fox');
-	    assert(model);
+
+	    assert.equal(4, model.entryCount());
 	});
     });
 });
