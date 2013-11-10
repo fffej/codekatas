b03df04b45b069a4bea75bed5a8428eb7b1275c2
Jeff Foster <jeff.foster@acm.org>
2013-11-10 14:08:05 +0000

    reformulate as recursion

--- WordSmushing/smushing.js
+++ WordSmushing/smushing.js
@@ -3,11 +3,13 @@
 var assert = require('assert');
 
 var smush = function(w1,w2) {
-    return w1 + w2;
+    if (w1 === '') return w2;
+
+    return w1[0] + smush(w1.substr(1), w2);
 };
 
 describe('word smushing', function() {
-    it('should join words with no overlap', function() {
+    it.only('should join words with no overlap', function() {
 	assert.equal('thecat', smush('the', 'cat'));
     });
 
