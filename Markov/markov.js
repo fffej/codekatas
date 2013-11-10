ce6bd472c1e9e6d67192f18e4a8568c5c0fcf6ce
Jeff Foster <jeff.foster@acm.org>
2013-11-10 14:43:29 +0000

    this isnt good code, but it passes the tests

--- Markov/markov.js
+++ Markov/markov.js
@@ -2,12 +2,27 @@
 
 var Model = function(tokens) {
 
+    var n = tokens.length;
+
+    this._data = [];
+
+    for (var i=0;i<n-1;++i) {
+	var token = tokens[i];
+
+	if (!this._data[token]) this._data[token] = {};
+
+	if (!this._data[token][tokens[i+1]])
+	    this._data[token][tokens[i+1]] = 0;
+
+	this._data[token][tokens[i+1]]++;
+    }
+
     this.entryCount = function() {
 	return tokens.length;
     };
 
     this.frequencyOf = function() {
-	return 1;
+	return this._data[arguments[0]][arguments[1]];
     };
 
     return this;
