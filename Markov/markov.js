b6b11ad10d290a1f62dfbeb5ff23d7c3920446cc
Jeff Foster <jeff.foster@acm.org>
2013-11-10 14:32:43 +0000

    simplest possible pass

--- Markov/markov.js
+++ Markov/markov.js
@@ -1,7 +1,15 @@
 "use strict";
 
+var Model = function(tokens) {
+    this.entryCount = function() {
+	return tokens.length;
+    };
+
+    return this;
+};
+
 var buildModel = function(source) { 
-    return source; 
+    return new Model(source); 
 };
 
 exports.buildModel = buildModel;
