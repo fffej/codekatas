acb211919c7d8b6b37aedc2687bf290bc87d056f
Jeff Foster <jeff.foster@acm.org>
2013-11-10 14:04:54 +0000

    test that fails

--- WordSmushing/smushing.js
+++ WordSmushing/smushing.js
@@ -10,4 +10,8 @@ describe('word smushing', function() {
     it('should join words with no overlap', function() {
 	assert.equal('thecat', smush('the', 'cat'));
     });
+
+    it('should do single character overlaps', function() {
+	assert.equal('henot', smush('hen', 'not'));
+    });
 });
