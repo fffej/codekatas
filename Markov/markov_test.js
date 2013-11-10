b089bac897835b859eff5e6397ac6f9a5a8eba07
Jeff Foster <jeff.foster@acm.org>
2013-11-10 14:34:58 +0000

    refactor to before each

--- Markov/markov_test.js
+++ Markov/markov_test.js
@@ -5,14 +5,19 @@ var markov = require('./markov.js');
 
 describe('Markov chain', function() {
     describe('building model', function() {
-	it('should provide entries', function() {
-	    var model = markov.buildModel([
+
+	var model = null;
+	
+	beforeEach(function() {
+	    model = markov.buildModel([
 		'the',
 		'quick',
 		'brown',
 		'fox'
 	    ]);
+	});
 
+	it('should provide entries', function() {
 	    assert.equal(4, model.entryCount());
 	});
     });
