6aecdd5f0ead02b2b7b9635f05e778905011e779
= <jeff.foster@acm.org>
2013-11-11 17:00:18 +0000

    minimal pass

--- BowlingGame.js
+++ BowlingGame.js
@@ -2,6 +2,17 @@
 
 var assert = require('assert');
 
+var Game = function() {
+    this.roll = function(n) {	
+    };
+
+    this.score = function() {
+	return 0;
+    };
+
+    return this;
+};
+
 describe('bowling game', function() {
     it('rolling 20 gutter balls is a gutter game', function() {
 	var game = new Game();
