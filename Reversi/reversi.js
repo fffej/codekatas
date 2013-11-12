93b0efa308ee9aa865805e2791c2bd38080b66b0
= <jeff.foster@acm.org>
2013-11-12 15:13:48 +0000

    better first failing test

--- Reversi/reversi.js
+++ Reversi/reversi.js
@@ -5,6 +5,6 @@ var assert = require('assert');
 describe('reversi', function() {
     it('should give no legal moves', function() {
 	var board = createBoard(['BW','BW']);
-	assert.equal(0,board.moves().length);
+	assert(board);
     });
 });
