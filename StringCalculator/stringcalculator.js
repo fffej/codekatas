4b2959a6c926771e92808bce9dd9fe082b848c27
Jeff Foster <jeff.foster@acm.org>
2013-11-18 20:54:15 +0000

    adding

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -15,4 +15,8 @@ describe('string calculator', function() {
 	assert.equal(1, add('1'));
 	assert.equal(3, add('3'));
     });
+
+    it('multiple numbers add', function() {
+	assert.equal(2, add('1,1'));
+    });
 });
