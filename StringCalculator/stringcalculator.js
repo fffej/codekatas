a627b687f7cc85824ba99c74f26a48fe5fc1b468
Jeff Foster <jeff.foster@acm.org>
2013-11-18 20:51:58 +0000

    first pass

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -2,6 +2,10 @@
 
 var assert = require('assert');
 
+var add = function() {
+    return 0;
+};
+
 describe('string calculator', function() {
     it('adding empty strings gives zero', function() {
 	assert.equal(0, add(''));
