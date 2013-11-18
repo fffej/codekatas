716de5765a707d9a591ddeeffbeb683de3747cfb
Jeff Foster <jeff.foster@acm.org>
2013-11-18 20:56:24 +0000

    rename test

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -19,7 +19,7 @@ describe('string calculator', function() {
 	assert.equal(3, add('3'));
     });
 
-    it('multiple numbers add', function() {
+    it('two numbers add', function() {
 	assert.equal(2, add('1,1'));
 	assert.equal(3, add('1,2'));
     });
