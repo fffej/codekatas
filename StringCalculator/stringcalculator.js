2304e8b3e4d6de10a09ef1674fbf279553656fc3
Jeff Foster <jeff.foster@acm.org>
2013-11-18 20:55:41 +0000

    make it pass in yucky sense

--- StringCalculator/stringcalculator.js
+++ StringCalculator/stringcalculator.js
@@ -3,7 +3,9 @@
 var assert = require('assert');
 
 var add = function(s) {
-    if (s.indexOf(',') !== -1) return 2;
+    var nums = s.split(',');
+    
+    if (s.indexOf(',') !== -1) return (nums[0]|0) + (nums[1]|0);
 
     return s|0;
 };
