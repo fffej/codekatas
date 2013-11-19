6df9ecc3d00f8410faf5453ef0e1f41a4dfc915b
Jeff Foster <jeff.foster@acm.org>
2013-11-19 08:07:16 +0000

    unskip

--- AStar/astar.js
+++ AStar/astar.js
@@ -167,7 +167,7 @@ describe('a* search algorithm', function() {
 	    assert.equal(2, moves.length);
 	});
 
-	it.skip('makes the best choice when confronted', function() {
+	it('makes the best choice when confronted', function() {
 	    var map = load('@*\n.X');
 	    var moves = [];
 
