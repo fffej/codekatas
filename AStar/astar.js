15ceb4748efeb86e52ab23fc1d6d50b3bb27b7fb
Jeff Foster <jeff.foster@acm.org>
2013-11-19 07:14:47 +0000

    for completeness

--- AStar/astar.js
+++ AStar/astar.js
@@ -10,6 +10,8 @@ describe('a* search algorithm', function() {
     describe('costs', function() {
 	it('flatlands cost 1', function() {
 	    assert.equal(1, cost('.'));
+	    assert.equal(1, cost('@'));
+	    assert.equal(1, cost('X'));
 	});
     });
 });
