047aa888b1fc74df733eda768f56f44346fdf962
Jeff Foster <jeff.foster@acm.org>
2013-11-20 07:55:08 +0000

    make pass.

--- circuit/circuit.js
+++ circuit/circuit.js
@@ -46,6 +46,16 @@ var And = function(input1,input2,output) {
     });
 };
 
+var Or = function(input1,input2,output) {
+    input1.onChange(function(s) {
+	output.setSignal(s || input2.getSignal());
+    });
+
+    input2.onChange(function(s) {
+	output.setSignal(s || input1.getSignal());
+    });
+};
+
 var makeWire = function() {
     return new Wire();
 };
@@ -56,6 +66,10 @@ var makeInverter = function(input,output) {
 
 var makeAnd = function(input1,input2,output) {
     return new And(input1,input2,output);
+};
+
+var makeOr = function(input1,input2,output) {
+    return new Or(input1,input2,output);
 };
 
 describe('circuit simulator', function() {
