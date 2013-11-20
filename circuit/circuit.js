16d41e987bd5a22f6c9d029c9432bd0ef7e3acb9
Jeff Foster <jeff.foster@acm.org>
2013-11-20 07:37:53 +0000

    need wires

--- circuit/circuit.js
+++ circuit/circuit.js
@@ -1,3 +1,13 @@
 "use strict";
 
 var assert = require('assert');
+
+describe('circuit simulator', function() {
+    it('wires have a signal', function() {
+	var w = makeWire();
+	assert.equal(false, w.getSignal());
+
+	w.setSignal(true);
+	assert.equal(true, w.getSignal());
+    });
+});
