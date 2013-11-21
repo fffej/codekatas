edc973274b4960311ba5aac6700a7e012fc62752
Jeff Foster <jeff.foster@acm.org>
2013-11-21 17:19:29 +0000

    woot, all tests pass

--- circuit/circuit.js
+++ circuit/circuit.js
@@ -45,12 +45,9 @@ var And = function(input1,input2,output) {
 };
 
 var Or = function(input1,input2,output) {
-    input1.onChange(function(s) {
-	output.setSignal(s || input2.getSignal());
-    });
-
-    input2.onChange(function(s) {
-	output.setSignal(s || input1.getSignal());
+    source.subscribe(function(wire) {
+	if (wire === input1 || wire === input2) 
+	    output.setSignal(input1.getSignal() || input2.getSignal());
     });
 };
 
