b513066f8f7e989c285389ad32970e8e67268714
Jeff Foster <jeff.foster@acm.org>
2013-11-21 17:15:55 +0000

    back to two failing tests

--- circuit/circuit_test.js
+++ circuit/circuit_test.js
@@ -12,7 +12,7 @@ describe('circuit simulator', function() {
 	assert.equal(true, w.getSignal());
     });
 
-    it.only('has an inverter', function() {
+    it('has an inverter', function() {
 	var input = circuit.makeWire();
 	var output = circuit.makeWire();
 
