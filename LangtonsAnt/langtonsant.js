e39fff7a3c074e08da1d3db0e58b68cecd25c599
Jeff Foster <jeff.foster@acm.org>
2013-11-22 16:06:08 +0000

    minimal viable pass

--- LangtonsAnt/langtonsant.js
+++ LangtonsAnt/langtonsant.js
@@ -1,6 +1,10 @@
 "use strict";
 var assert = require('assert');
 
+var makeAnt = function() {
+    return {};
+};
+
 describe('langton\'s ant', function() {
     describe('ant', function() {
 	it('exists', function() {
