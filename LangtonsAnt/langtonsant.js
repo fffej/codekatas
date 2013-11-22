a6402e5f62002e3676d42b4bfaea61e1d9fff976
Jeff Foster <jeff.foster@acm.org>
2013-11-22 16:06:59 +0000

    failing test

--- LangtonsAnt/langtonsant.js
+++ LangtonsAnt/langtonsant.js
@@ -10,5 +10,11 @@ describe('langton\'s ant', function() {
 	it('exists', function() {
 	    assert(makeAnt());
 	});
+
+	it('has a location', function() {
+	    var ant = makeAnt();
+	    assert.equal(0, ant.getPos().x);
+	    assert.equal(0, ant.getPos().y);
+	});
     });
 });
