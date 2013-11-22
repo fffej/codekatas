ae2af5bd698363883793071fe34a13d159470d0d
Jeff Foster <jeff.foster@acm.org>
2013-11-22 16:05:51 +0000

    first failing test

---
+++ LangtonsAnt/langtonsant.js
@@ -0,0 +1,10 @@
+"use strict";
+var assert = require('assert');
+
+describe('langton\'s ant', function() {
+    describe('ant', function() {
+	it('exists', function() {
+	    assert(makeAnt());
+	});
+    });
+});
