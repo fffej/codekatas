6cef0a06f63336c2bf1b564fc7432393b1e90535
Jeff Foster <jeff.foster@acm.org>
2013-11-26 13:46:41 +0000

    refactor

--- MineField/minefield.js
+++ MineField/minefield.js
@@ -5,16 +5,12 @@ var assert = require('assert');
 var surroundingMines = function(rows) {
     
     var mineCount = function(i,j) {
-	assert(i !== undefined)
-	assert(j !== undefined)
-
 	var count = 0;
-
 	for (var xd=-1;xd<=1;xd++) 
-	    for (var yd=-1;yd<=1;yd++) {
+	    for (var yd=-1;yd<=1;yd++) 
 		if (rows[xd+i] && rows[xd+i][yd+j] === '*')
 		    count++;
-	    }
+
 	return count;
     };
 
