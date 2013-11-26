a4a3929bb80cd3a686b072fa64596d1881cc9590
Jeff Foster <jeff.foster@acm.org>
2013-11-26 13:24:01 +0000

    first failing test

---
+++ MineField/minefield.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var assert = require('assert');
+
+var produceMineField = function(s) {
+    return '';
+};
+
+describe('mine field', function() {
+    it('works for a single mine field', function() {
+	assert.equal('*', produceMineField('*'));
+    });
+});
