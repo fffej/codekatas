1eb32c9f11f09de7b6c475158ae1ffc853512b79
Jeff Foster <jeff.foster@acm.org>
2013-11-26 13:30:54 +0000

    make pass

--- MineField/minefield.js
+++ MineField/minefield.js
@@ -3,7 +3,8 @@
 var assert = require('assert');
 
 var produceMineField = function(s) {
-    return s === '*' ? '*' : '0';
+    var rows = s.split('\n');
+    return rows.map(function(x) { return x === '*' ? '*' : '0'; }).join('\n');
 };
 
 describe('mine field', function() {
@@ -16,6 +17,6 @@ describe('mine field', function() {
     });
 
     it('works for a 1x2 empty field', function() {
-	assert.equal('0\n0\n', produceMineField('.\n.'));
+	assert.equal('0\n0', produceMineField('.\n.'));
     });
 });
