c48a6b47ebfd63ae7c2970d6bb519bea7d74ad13
Jeff Foster <jeff.foster@acm.org>
2013-12-10 08:16:49 +0000

    description

--- POFAD/smallestfreenumber/SmallestFreeNumber.hs
+++ POFAD/smallestfreenumber/SmallestFreeNumber.hs
@@ -1,6 +1,12 @@
+{-
+Consider the problem of computing the smallest natural number not
+in a given finite set X of natural numbers.
+-}
+
 module SmallestFreeNumber where 
 
 import Data.List ((\\))
 
+-- Naive definition.  This is O(N^2)
 minFree :: [Integer] -> Integer
 minFree xs = head ([0 .. ] \\ xs)
