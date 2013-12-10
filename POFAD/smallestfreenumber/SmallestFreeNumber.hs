7744ea5e29717fc8cc3c30a5c553e1556a83306d
Jeff Foster <jeff.foster@acm.org>
2013-12-10 08:27:26 +0000

    much more efficient version

--- POFAD/smallestfreenumber/SmallestFreeNumber.hs
+++ POFAD/smallestfreenumber/SmallestFreeNumber.hs
@@ -6,7 +6,25 @@ in a given finite set X of natural numbers.
 module SmallestFreeNumber where 
 
 import Data.List ((\\))
+import Data.Array
 
 -- Naive definition.  This is O(N^2)
 minFree :: [Integer] -> Integer
 minFree xs = head ([0 .. ] \\ xs)
+
+-- Key fact: Not every number in the range [0 .. length xs] 
+-- can be in xs
+search :: Array Int Bool -> Int
+search = length . takeWhile id . elems
+
+-- accumArray is mental
+-- arg one transforms array entries and values into new entries
+-- arg two is initial index
+-- arg three defines lower and upper bounds
+-- arg fourth is an association list
+
+checklist :: [Int] -> Array Int Bool
+checklist xs = accumArray (||) False (0,n) (zip numsLessThanN (repeat True))
+    where
+        n = length xs
+        numsLessThanN = filter (<= n) xs	       
