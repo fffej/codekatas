3cdba1084d43b6fe2211f9b470749261c8ad6590
Jeff Foster <jeff.foster@acm.org>
2013-12-13 07:27:18 +0000

    first failing test

--- BowlingGame/BowlingGame.hs
+++ BowlingGame/BowlingGame.hs
@@ -3,5 +3,11 @@ module BowlingGame where
 import Test.Hspec
 import Test.QuickCheck
 
+score :: [Int] -> Int
+score = undefined
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = hspec $ do
+  describe "Gutter game" $ do
+    it "rolling 20 zeros scores 0" $ do
+      score (replicate 20 0) `shouldBe` 0
