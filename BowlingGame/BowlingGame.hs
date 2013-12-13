ccb69097958d620a16abd609b075a771bc099ee2
Jeff Foster <jeff.foster@acm.org>
2013-12-13 07:28:08 +0000

    failing secondary test

--- BowlingGame/BowlingGame.hs
+++ BowlingGame/BowlingGame.hs
@@ -11,3 +11,5 @@ main = hspec $ do
   describe "Gutter game" $ do
     it "rolling 20 zeros scores 0" $ do
       score (replicate 20 0) `shouldBe` 0
+    it "rolling 20 single pins scores 20" $ do
+      score (replicate 20 1) `shouldBe` 20
