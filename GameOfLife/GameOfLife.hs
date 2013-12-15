17f0d0c12fdb66f50b61d166b277234ab88735db
Jeff Foster <jeff.foster@acm.org>
2013-12-15 09:46:59 +0000

    quickchcek not appropriate

--- GameOfLife/GameOfLife.hs
+++ GameOfLife/GameOfLife.hs
@@ -11,7 +11,7 @@ nextState Live 2 = Dead
 main :: IO ()
 main = hspec $ do
   describe "Game of life" $ do
-    it "A live cell with 2 or less neighbours, dies" $ property
-      (\n -> n >=0 && n <=2 ==> nextState Live n == Dead)
+    it "A live cell with 2 or less neighbours, dies" $ do 
+      all (\n -> nextState Live n == Dead) [0,1,2]
     
 
