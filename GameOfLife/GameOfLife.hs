f897a6d0a580624ed152cd804875e1f4248d536d
Jeff Foster <jeff.foster@acm.org>
2013-12-15 09:55:57 +0000

    eliminate duplication

--- GameOfLife/GameOfLife.hs
+++ GameOfLife/GameOfLife.hs
@@ -3,6 +3,8 @@ module GameOfLife where
 import Test.Hspec
 import Test.QuickCheck
 
+import Data.List ((\\))
+
 data CellState = Live | Dead deriving (Show,Eq)
 
 nextState :: CellState -> Int -> CellState
@@ -13,9 +15,8 @@ nextState Live n
 main :: IO ()
 main = hspec $ do
   describe "Game of life" $ do
-    it "A live cell fewer than 2 live neighbours, dies" $ do 
-      all (\n -> nextState Live n == Dead) [0,1]
+    it "A live cell with anything other than 2 or 3 neighbours, dies" $ do 
+      all (\n -> nextState Live n == Dead) ([0..9] \\ [2,3])
     it "A live cell with 2 or 3 live neighbours live on" $ do
       all (\n -> nextState Live n == Live) [2,3]
-    it "A live cell with more than 3 live neighbours dies" $ do
-      all (\n -> nextState Live n == Dead) [4..9]
+
