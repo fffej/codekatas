47866c34197096123a8e3a164f4687e6898db74b
Jeff Foster <jeff.foster@acm.org>
2013-12-15 09:53:11 +0000

    minimal viable pass

--- GameOfLife/GameOfLife.hs
+++ GameOfLife/GameOfLife.hs
@@ -7,7 +7,7 @@ data CellState = Live | Dead deriving (Show,Eq)
 
 nextState :: CellState -> Int -> CellState
 nextState Live n
-  | n < 2 = Dead
+  | n < 2 || n > 3 = Dead
   | otherwise = Live
 
 main :: IO ()
