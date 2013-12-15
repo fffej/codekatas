90e32bb90569f4479b08dc6a131c110e8475216a
Jeff Foster <jeff.foster@acm.org>
2013-12-15 09:43:41 +0000

    encode it with a property

--- GameOfLife/GameOfLife.hs
+++ GameOfLife/GameOfLife.hs
@@ -12,6 +12,6 @@ main :: IO ()
 main = hspec $ do
   describe "Game of life" $ do
     it "A live cell with 2 or less neighbours, dies" $ property
-      (\n -> nextState Live n == Dead) 
+      (\n -> n >=0 && n <=2 ==> nextState Live n == Dead)
     
 
