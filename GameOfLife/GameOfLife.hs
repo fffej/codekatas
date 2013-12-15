72b1fb4ef03415584a92585e11ddf8f33af51751
Jeff Foster <jeff.foster@acm.org>
2013-12-15 09:39:04 +0000

    start to promote this to a quick check pattern, currently failing

--- GameOfLife/GameOfLife.hs
+++ GameOfLife/GameOfLife.hs
@@ -11,7 +11,7 @@ nextState Live 2 = Dead
 main :: IO ()
 main = hspec $ do
   describe "Game of life" $ do
-    it "A live cell with 2 or less neighbours, dies" $ do
-      nextState Live 2 `shouldBe` Dead
+    it "A live cell with 2 or less neighbours, dies" $ property
+      (\n -> nextState Live n == Dead) 
     
 
