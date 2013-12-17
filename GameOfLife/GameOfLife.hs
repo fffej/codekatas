1872bffa19e5e4919418079301c2942f927c6cd8
Jeff Foster <jeff.foster@acm.org>
2013-12-17 08:01:16 +0000

    step back up, write failing test for live neighbours

--- GameOfLife/GameOfLife.hs
+++ GameOfLife/GameOfLife.hs
@@ -25,6 +25,9 @@ mkGrid w h = M.fromList [((x,y),Dead) | x <- [0..w], y <- [0..h]]
 cellAt :: Grid -> Point -> CellState
 cellAt g p = g M.! p
 
+liveNeighbours :: Grid -> Point -> Int
+liveNeighbours = undefined
+
 neighbours :: Point -> [Point]
 neighbours (x,y) = [(x+dx,y+dy) | dx <- [-1,0,1], dy <- [-1,0,1] ]
 
@@ -47,11 +50,6 @@ tickCell' xs s = if (elem s xs) then Live else Dead
 nextState :: CellState -> Int -> CellState
 nextState = tickCell defaultRules
 
-tickGrid :: Rules -> Grid -> Grid
-tickGrid rules grid = M.mapWithKey (\p c -> tickCell rules c liveNeighbours) grid
-  where
-    liveNeighbours = 0
-  
 main :: IO ()
 main = hspec $ do
   describe "Game of life" $ do
@@ -70,7 +68,5 @@ main = hspec $ do
       all (\(x,y) -> x >= 0 && x < 10) (map (bound (10,10)) (neighbours (0,0)))
     it "grids are initially all dead" $ do
       all (\x -> cellAt (mkGrid 3 3) x == Dead) [(0,0),(1,1)]
-        
-
-      
-      
+    it "live neighbours can be retrieved" $ do
+      liveNeighbours (mkGrid 3 3) (0,0) == 0
