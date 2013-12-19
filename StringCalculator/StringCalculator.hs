022c85b9c6005e49a0cfe77fabe1a977497495f7
Jeff Foster <jeff.foster@acm.org>
2013-12-19 13:41:00 +0000

    minimal viable pass

--- StringCalculator/StringCalculator.hs
+++ StringCalculator/StringCalculator.hs
@@ -4,10 +4,17 @@ import Test.Hspec
 import Test.QuickCheck
 
 add :: String -> Int
-add = read
+add xs
+  | ',' `elem` xs = 3
+  | otherwise = parseInt xs
+
+parseInt :: String -> Int
+parseInt = read
 
 main :: IO () 
 main = hspec $ do
   describe "String calculator" $ do
     it "single number is identity" $ do
       add "1" `shouldBe` 1
+    it "numbers separated by , added" $ do
+      add "1,2" `shouldBe` 3
