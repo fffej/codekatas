061ccecfd7921a10b38cac2ed7b40ece3deca3fb
Jeff Foster <jeff.foster@acm.org>
2013-12-19 13:46:48 +0000

    split

--- StringCalculator/StringCalculator.hs
+++ StringCalculator/StringCalculator.hs
@@ -7,7 +7,7 @@ import Test.QuickCheck
 
 add :: String -> Int
 add xs
-  | ',' `elem` xs = sum (map parseInt (splitOn "," xs))
+  | ',' `elem` xs = sum (map parseInt (splitWhen (\x -> x == ',' || x == '\n') xs))
   | otherwise = parseInt xs
 
 parseInt :: String -> Int
@@ -22,3 +22,5 @@ main = hspec $ do
       add "1,2" `shouldBe` 3
     it "any number of numbers" $ do
       add "1,2,3,4,5" `shouldBe` 15
+    it ", and \n are allowed" $ do
+      add "1\n2,3" `shouldBe` 6
