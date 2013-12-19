2ab8f62312a775edc96ad1e0e267ae8842da4db8
Jeff Foster <jeff.foster@acm.org>
2013-12-19 13:56:51 +0000

    format message better

--- StringCalculator/StringCalculator.hs
+++ StringCalculator/StringCalculator.hs
@@ -34,7 +34,7 @@ main = hspec $ do
       add "1,2" `shouldBe` 3
     it "any number of numbers" $ do
       add "1,2,3,4,5" `shouldBe` 15
-    it ", and \n are allowed" $ do
+    it ", and \\n are allowed" $ do
       add "1\n2,3" `shouldBe` 6
     it "supports custom delimiters" $ do
       add "//:\n1:2" `shouldBe` 3
