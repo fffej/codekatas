b94539316a32ea96f82e47f1bd65d87aaddabee7
Jeff Foster <jeff.foster@acm.org>
2013-12-24 07:20:31 +0000

    failing test

--- 99Problems/21-30/Problems.hs
+++ 99Problems/21-30/Problems.hs
@@ -7,7 +7,11 @@ insertAt :: a -> [a] -> Int -> [a]
 insertAt e xs 1      = e:xs
 insertAt e (x:xs) n  = x : insertAt e xs (n - 1)
 
+range = undefined
+
 main = hspec $ do
   describe "99 problems" $ do
     it "should insert at" $ do
       insertAt 'X' "abcd" 2 `shouldBe` "aXbcd"
+    it "should define range" $ do
+      range 4 9 `shouldBe` [4,5,6,7,8,9]
