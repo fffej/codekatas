8766992b9bcac48d7397a41706bd6510e819c8ac
Jeff Foster <jeff.foster@acm.org>
2013-12-24 13:31:13 +0000

    failing test case

--- 99Problems/31-41/Problems.hs
+++ 99Problems/31-41/Problems.hs
@@ -6,6 +6,8 @@ import Test.QuickCheck
 isPrime :: Integer -> Bool
 isPrime n = null [ x | x <- [2.. n-1], n `mod` x == 0]
 
+gcd' = undefined
+
 main :: IO ()
 main = hspec $ do
   describe "Arithmetic" $ do
@@ -13,3 +15,5 @@ main = hspec $ do
       isPrime 7
     it "can detect primes (negative)" $ do
       not (isPrime 345)
+    it "can do gcd" $ do
+      [gcd' 36 63, gcd' (-3) (-6), gcd' (-3) 6] `shouldBe` [9,3,3]
