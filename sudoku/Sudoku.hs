257d51ddca09a9cbd513efced0d89c10ae3a0fff
Jeff Foster <jeff.foster@acm.org>
2013-12-31 13:40:07 +0000

    these would seem to be obviously needed functions

--- sudoku/Sudoku.hs
+++ sudoku/Sudoku.hs
@@ -9,9 +9,21 @@ data Cell = Unknown [Int]
           | Fixed Int
             deriving (Show,Eq)
 
+type Grid = Array (Int,Int) Cell
+
+grid :: Grid
 grid = listArray bounds (repeat $ Unknown [1..9])
   where
     bounds :: ((Int,Int),(Int,Int))
     bounds = ((0,0),(8,8))
+
+row :: Grid -> (Int,Int) -> [Cell]
+row g (r,c) = undefined
+
+col :: Grid -> (Int,Int) -> [Cell]
+col g (r,c) = undefined
+
+subgrid :: Grid -> (Int,Int) -> [Cell]
+subgrid g (r,c) = undefined
 
 main = undefined
