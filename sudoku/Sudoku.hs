b5a7681e66688959348d932031463f01454366bc
Jeff Foster <jeff.foster@acm.org>
2013-12-31 13:37:26 +0000

    define a sudoku board

--- sudoku/Sudoku.hs
+++ sudoku/Sudoku.hs
@@ -3,4 +3,15 @@ module Sudoku where
 import Test.QuickCheck
 import Test.Hspec
 
+import Data.Array
+
+data Cell = Unknown [Int]
+          | Fixed Int
+            deriving (Show,Eq)
+
+grid = listArray bounds (repeat $ Unknown [1..9])
+  where
+    bounds :: ((Int,Int),(Int,Int))
+    bounds = ((0,0),(8,8))
+
 main = undefined
